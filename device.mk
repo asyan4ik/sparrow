91706e767f8f7448a6a3a440811809ee61a6a075
Vladimir <acbkaa@gmail.com>
2016-04-08 14:38:18 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product-if-exists, vendor/asus/anthias/anthias-vendor.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/asus/sparrow/sparrow-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/sparrow/overlay
 
@@ -12,9 +16,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.sparrow:root/fstab.sparrow \
 
 $(call inherit-product, build/target/product/full.mk)
 
